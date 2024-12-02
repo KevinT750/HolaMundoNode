8b6c0fe120e08b65ed40fe7598ccccb724800ccc
Kevin750xd <kevinteran750@gmail.com>
2024-12-02 09:18:52 -0500

    index

---
+++ index.js
@@ -0,0 +1,16 @@
+//Protocolo de INtercambio
+var http = require("http");
+var server = http.createServer();
+
+//SERVICIO WEB
+function mensaje(req, resp) {
+    //ESTADO SATISFACTORIO 200, 
+    resp.writeHead(200, { 'Content-Type': 'text/plain' }); 
+    resp.write('Hola Mundo'); 
+    resp.end(); 
+}
+
+server.on('request' , mensaje);
+server.listen(3000, function(){
+    console.log('La solicitud fue realizasda mediante el puerto 3000')
+});
